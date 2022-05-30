13cbf7506d3832d41ebc02fed38243efe6e172d7
Andrian Yu <76722207+yuandrian@users.noreply.github.com>
2022-05-30 11:20:23 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const petsData = [
+  {
+    name: "Purrsloud",
+    species: "Cat",
+    favFoods: ["wet food", "dry food", "<strong>any</strong> food"],
+    birthYear: 2017,
+    photo: "https://learnwebcode.github.io/json-example/images/cat-2.jpg",
+  },
+  {
+    name: "Barksalot",
+    species: "Dog",
+    birthYear: 2008,
+    photo: "https://learnwebcode.github.io/json-example/images/dog-1.jpg",
+  },
+  {
+    name: "Meowsalot",
+    species: "Cat",
+    favFoods: ["tuna", "catnip", "celery"],
+    birthYear: 2012,
+    photo: "https://learnwebcode.github.io/json-example/images/cat-1.jpg",
+  },
+];
+
+function petTemplate(pet){
+    return `<div class="animal">
+    <img class="pet-photo" src="${pet.photo}">
+    <h2 class="pet-name">${pet.name}<span class="species">(${pet.species})</span></h2>
+</div>`;
+}
+
+document.getElementById("app").innerHTML = `<h1>Pets(${petsData.length} results)</h1>
+${petsData.map(petTemplate).join('')}
+<p class="footer">These ${petsData.length} pets were added recently. Check back sppn fro updates.</p>`;
